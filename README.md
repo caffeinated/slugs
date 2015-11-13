684952544872b9cc89a26d5030a0e1fd9e2d3c7b
Shea Lewis <shea.lewis89@gmail.com>
2015-11-12 18:54:34 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# slugs
-Unique slug generator for your Eloquent models
+# Caffeinated Slugs
+[![Laravel](https://img.shields.io/badge/Laravel-5.1-orange.svg?style=flat-square)](http://laravel.com)
+[![Source](http://img.shields.io/badge/source-caffeinated/slugs-blue.svg?style=flat-square)](https://github.com/caffeinated/slugs)
+[![License](http://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](https://tldrlegal.com/license/mit-license)
+
+Caffeinated Slugs is still in a very early WIP stage.
+
+## Quick Installation
+Begin by installing the package through Composer. Add `caffeinated/slugs` to your composer.json file:
+
+```
+"caffeinated/slugs": "~1.0@dev"
+```
+
+Then run `composer update` to pull the package in.
+
+And that's it! With your coffee in reach, start building out some unique slugs!
---
+++ src/Traits/Sluggable.php
@@ -0,0 +1,62 @@
+<?php
+namespace Caffeinated\Slugs\Traits;
+
+trait Sluggable
+{
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function($model) {
+            $slugName  = static::getSlugName();
+            $slugField = static::getSlugField();
+
+            $model->$slugField = str_slug($model->$slugName);
+
+            $latestSlug = static::whereRaw("{$slugField} RLIKE '^{$model->slug}(-[0-9]*)?$'")
+                ->latest('id')
+                ->value($slugField);
+
+            if ($latestSlug) {
+                $pieces = explode('-', $latestSlug);
+
+                $number = intval(end($pieces));
+
+                $model->$slugField .= '-'.($number + 1);
+            }
+        });
+    }
+
+    /**
+     * Get the name field associated for slugs.
+     *
+     * @return string
+     */
+    public static function getSlugName()
+    {
+        if (null !== static::$slug['name']) {
+            return static::$slug['name'];
+        }
+
+        return 'name';
+    }
+
+    /**
+     * Get the slug field associated for slugs.
+     *
+     * @return string
+     */
+    public static function getSlugField()
+    {
+        if (null !== static::$slug['field']) {
+            return static::$slug['field'];
+        }
+
+        return 'slug';
+    }
+}
